7ff8eebec63e1cea452a2723b6dcca8bd22bdce9
Come-from-Beyond <come-from-beyond@mail.ru>
2014-09-25 01:12:41 +0300

    Java: Environment

---
+++ Java/src/cfb/jiniri/model/Environment.java
@@ -0,0 +1,43 @@
+package cfb.jiniri.model;
+
+import cfb.jiniri.type.Multiplet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * (c) 2014 Come-from-Beyond
+ */
+public class Environment {
+
+    private final Multiplet id;
+
+    private final Set<Multiplet> entityIds;
+
+    public Environment(final Multiplet id) {
+
+        this.id = id.clone();
+
+        entityIds = new HashSet<>();
+    }
+
+    public Multiplet getId() {
+
+        return id;
+    }
+
+    synchronized public boolean include(final Multiplet entityId) {
+
+        return entityIds.add(entityId);
+    }
+
+    synchronized public boolean exclude(final Multiplet entityId) {
+
+        return entityIds.remove(entityId);
+    }
+
+    synchronized public boolean contains(final Multiplet entityId) {
+
+        return entityIds.contains(entityId);
+    }
+}
