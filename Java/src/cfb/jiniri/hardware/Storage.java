604fe876c49269ea153a98dc60241521dd0f8c11
Come-from-Beyond <come-from-beyond@mail.ru>
2014-09-25 09:57:45 +0300

    Java: Storage

---
+++ Java/src/cfb/jiniri/hardware/Storage.java
@@ -0,0 +1,27 @@
+package cfb.jiniri.hardware;
+
+import cfb.jiniri.ternary.Tryte;
+
+/**
+ * 2014 Come-from-Beyond
+ */
+public interface Storage {
+
+    public void beginStoring(final Tryte time);
+
+    public void storeEntity(final Tryte[] entityTrytes);
+
+    public void storeEnvironment(final Tryte[] environmentTrytes);
+
+    public void storeEffect(final Tryte[] effectTrytes);
+
+    public void endStoring();
+
+    public Tryte beginLoading();
+
+    public Tryte[] loadEntity();
+
+    public Tryte[] loadEnvironment();
+
+    public Tryte[] loadEffect();
+}
