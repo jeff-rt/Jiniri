d29012888248c7416129b937767b7d20a9cf2a66
Come-from-Beyond <come-from-beyond@mail.ru>
2014-09-28 11:46:03 +0300

    Java: Set, Go and Call

---
+++ Java/src/cfb/jiniri/operation/Set.java
@@ -0,0 +1,11 @@
+package cfb.jiniri.operation;
+
+import cfb.jiniri.ternary.Trit;
+
+/**
+ * (c) 2014 Come-from-Beyond
+ */
+public class Set {
+
+    public static final Trit OPCODE = Trit.UNKNOWN;
+}
