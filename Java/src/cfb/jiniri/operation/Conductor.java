b6f48563aab1a2206ae9c658ec86809e5fb1aedb
Come-from-Beyond <come-from-beyond@mail.ru>
2014-09-29 18:19:04 +0300

    Java: Conductor

---
+++ Java/src/cfb/jiniri/operation/Conductor.java
@@ -0,0 +1,29 @@
+package cfb.jiniri.operation;
+
+import cfb.jiniri.type.Nonet;
+import cfb.jiniri.type.Singlet;
+
+/**
+ * (c) 2014 Come-from-Beyond
+ */
+public interface Conductor {
+
+    public void halt();
+
+    public void create(final Nonet type, final Singlet pointer, final Singlet size);
+
+    public void decay();
+
+    public void affect(final Nonet environmentId, final Singlet delay, final Singlet duration,
+                       final Singlet pointer, final Singlet size);
+
+    public void join(final Nonet environmentId);
+
+    public void leave(final Nonet environmentId);
+
+    public void distance(final Nonet environmentId, final Singlet distance);
+
+    public void approach(final Nonet environmentId, final Singlet delta);
+
+    public void retreat(final Nonet environmentId, final Singlet delta);
+}
